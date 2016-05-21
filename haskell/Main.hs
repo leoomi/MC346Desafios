a5a585612ec51aef63df0db113c1a0713ad43e3e
leo <leoyuuki@gmail.com>
2016-05-21 02:56:25 -0300

    Correct, albeit slow, version

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -4,9 +4,19 @@ import Rect
 main :: IO ()
 main = do
   contents <- getContents
-  putStrLn $ process $ readMultipleRects $ lines contents
+  --putStrLn $ rectListToString $ readMultipleRects $ contents
+  putStrLn $ show $ test $ readMultipleRects $ contents
 
-process :: [Rect] -> String
-process = unlines.map(""++)
---getCompatibleRect :: Rect -> [Rect] -> [Rect]
---getCompatibleRect (Rect ) list = filter (\) list
+test list = maximum $ map (`maxSum` list) list
+
+findCompatibles :: Rect -> [Rect] -> [Rect]
+findCompatibles x list = filter (`compatible` x) list
+
+maxSum :: Rect -> [Rect] -> Int
+maxSum x list
+  | comp == [] = area x 
+  | otherwise  = (area x) + (maximum $ map (`maxSum` comp) comp)
+  where comp = findCompatibles x list
+
+-- Rect 1 3 5 2 10 (16)
+-- [Rect 2 9 13 13 16 (12) ,Rect 5 16 20 11 14 (12),Rect 7 23 27 13 14 (4)]
--- haskell/Rect.hs
+++ haskell/Rect.hs
@@ -2,21 +2,40 @@ module Rect
        (
          Rect,
          readRect,
-         readMultipleRects
+         readMultipleRects,
+         area,
+         compatible,
+         --debug functions from here on out
+         rectListToString
        )where
 
---               id  xmin xmax ymin ymax area
-data Rect = Rect Int Int  Int  Int  Int  Int
-            deriving (Show) -- for testing
+--               id  xmin xmax ymin ymax
+data Rect = Rect Int Int  Int  Int  Int
+            deriving (Eq, Show)
 
+-- reads a single string line that represents a rectangle and returns its correspondent Rect type variable
 readRect :: String -> Rect
-readRect input = (Rect id xmin xmax ymin ymax area)
-  where (id:xmin:xmax:ymin:ymax:_) = [ read x :: Int | x <- (words input)]
-        area = ((xmax - xmin)*(ymax - ymin))
+readRect input = (Rect id xmin xmax ymin ymax)
+  where (id:xmin:xmax:ymin:ymax:_) = [ read x :: Int | x <- (words input) ]
 
+-- reads a string with multiple lines and runs the read Rect on each one, returning a Rect type variable
 readMultipleRects :: String -> [Rect]
 readMultipleRects input = map (readRect) (lines input)
 
+-- calculates the area of a Rect variable
+area :: Rect -> Int
+area (Rect _ xmin xmax ymin ymax) = (xmax - xmin) * (ymax - ymin)
 
---compatibleRect :: Rect -> Rect -> Bool
---compatibleRect a b =
+-- reads two Rect variables. Returns True if both variables are compatibles, that is, if one variable has all of its X coordinates smallers than the others, and all of its Y coordinates as well, without intersection. Returns False otherwise
+compatible :: Rect -> Rect -> Bool
+compatible (Rect _ xminA xmaxA yminA ymaxA) (Rect _ xminB xmaxB yminB ymaxB)
+  | (xmaxA < xminB) && (ymaxA < yminB) = True
+  | (xmaxB < xminA) && (ymaxB < yminA) = True
+  | otherwise = False
+
+-- Returns a Rect with area of 0 and the minimal coordinates
+minimalRect = let min = minBound :: Int in (Rect 0 min min min min)
+
+--debug functions
+rectListToString list =  unlines $ map (show) list
+
